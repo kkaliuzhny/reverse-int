a4516cda8b97f455e9ed01a9dc06c5ebf09bae24
Klim <kkaliuzhy@gmail.com>
2023-02-12 13:38:53 +0300

    feat:made a reverse task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (a) {
+    a=Math.abs(a);
+   let str= a.toString();
+    str=str.split("").reverse().join("");
+    str=Number(str);
+    
+    return str ;
 }
